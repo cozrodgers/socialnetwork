2151a8eefcc85ad7794c7d96a256bfa2c47c8c8e
Corey <coreyrodgers95@gmail.com>
2019-03-19 18:15:17 +0000

    add foundation

---
+++ socialnet/resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.master')
+
+
+<!--define the title section -->
+@section('title')
+    Welcome!
+@endsection
+
+@section('content')
+    <h1>Welcome to the socialnet</h1>
+@endsection
