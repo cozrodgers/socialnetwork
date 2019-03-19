efbe69ec285df3c61915063177fd684685b8c3f2
Corey <coreyrodgers95@gmail.com>
2019-03-19 22:18:28 +0000

    add user model, migration for user,complete basic sign up view

--- socialnet/resources/views/layouts/master.blade.php
+++ socialnet/resources/views/layouts/master.blade.php
@@ -3,17 +3,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="css/app.css">
     <title>@yield('title')</title>
 </head>
 <body>
 
-<div class="container">
+    @include('includes.header')
 
+<div class="container">
+        
+    
     @yield('content')
  
-    </div>
-    
+</div>
+    <!-- bootstrap js-->
+
 </div>
 </body>
 </html>
--- socialnet/resources/views/welcome.blade.php
+++ socialnet/resources/views/welcome.blade.php
@@ -1,11 +1,54 @@
 @extends('layouts.master')
 
-
 <!--define the title section -->
 @section('title')
     Welcome!
 @endsection
 
+
+
 @section('content')
-    <h1>Welcome to the socialnet</h1>
+
+
+        <div class="row">
+
+                 <div class="col-md-6">
+                     <h3>Sign Up</h3>
+                         <form action="#" method="post">
+ 
+                                 <div class="form-group">
+                                    <label for="email">Your Email</label>
+                                    <input type="text" name="email" class="form-control">
+                                    <label for="first_name">Your First Name</label>
+                                    <input type="text" name="first_name" class="form-control">
+                                    <label for="email">Your Last Name</label>
+                                    <input type="text" name="last_name" class="form-control">
+                                
+                                             
+                                 </div>
+                             <button type="submit" class="btn btn-primary">Submit</button>
+                         </form>
+ 
+                 </div>
+
+                 <div class="col-md-6">
+                    <h3>Sign in</h3>
+                        <form action="#" method="post">
+
+                                <div class="form-group">
+                                   <label for="email">Your Email</label>
+                                   <input type="text" name="email" class="form-control">
+                                   <label for="email">Your Password</label>
+                                   <input type="text" name="password" class="form-control">
+                                            
+                                </div>
+                            <button type="submit" class="btn btn-success">Submit</button>
+                        </form>
+
+                </div> 
+        </div>
+ 
+
+     
+   
 @endsection
